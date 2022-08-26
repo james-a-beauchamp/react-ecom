b4ddec86b39b8f1b725e412c96672a658665b568
James Beauchamp <james.phx@gmail.com>
2022-08-26 07:35:53 -0700

    Added basic Stripe credit card integration

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,9 +1,12 @@
 import { useSelector } from 'react-redux';
+
 import {
   selectCartItems,
   selectCartTotal,
 } from '../../store/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
+
 import {
   CheckoutContainer,
   CheckoutHeader,
@@ -39,6 +42,7 @@ const Checkout = () => {
         return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
       })}
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
